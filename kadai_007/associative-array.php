4bdcd2d104ff967f4f5b2bff71bc8d735768f58c
HI89KURA <137121785+HI89KURA@users.noreply.github.com>
2023-09-11 21:38:25 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $associative = ['name' => 'onion', 'price' => 200, 'wight' => 160];
+
+
+    print_r($associative);
+
+    
+    ?>
+  </p>
+  
+</body>
+</html>
